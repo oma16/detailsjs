aae1d43966301d14b93c1ef88abfe95ec4b18e58
oma16 <48384861+oma16@users.noreply.github.com>
2022-06-21 11:36:26 +0100

    details

---
+++ index.js
@@ -0,0 +1,12 @@
+let Name = "Alli Mariam Titilope";
+let Height = 1.59;
+let country = "Nigeria";
+
+
+document.getElementById("details").style.color = "blue";
+document.getElementById("details").style.textAlign = "center";
+document.getElementById("details").style.marginTop = "80px";
+document.getElementById("details").style.fontSize = "larger";
+document.getElementById("names").innerHTML = "Name: " + Name;
+document.getElementById("heights").innerHTML = "Height: " + Height + "m";
+document.getElementById("ctry").innerHTML = "Country: " + country;
